d9f75340cdd3716bdae58bbb72b72b5fddc1fe8a
Nerossoul <nerossoul@gmail.com>
2019-02-11 01:31:43 +0700

    test auth created

--- src/components/settings_vk_auth.vue
+++ src/components/settings_vk_auth.vue
@@ -4,14 +4,14 @@
     <div class="card-body">
       <h5 class="card-title">Авторизация в контакте:</h5>
       <p class="card-text">{{user_auth_link_to_get_code}}</p>
-      <a type="button" name="button" class="btn btn-dark">Авторизоваться</a>
+      <a type="button" :href="user_auth_link_to_get_code" name="button" target="_blank" class="btn btn-dark">Get code</a>
+      <a type="button" :href="group_auth_link_to_get_access_token" name="button" target="_blank" class="btn btn-dark">Get access token</a>
     </div>
   </div>
 </div>
 </template>
 
 <script>
-/* eslint-disable */
 
 export default {
   name: 'settings_vk_auth',
@@ -19,7 +19,8 @@ export default {
     return {
       user_auth_link_to_get_code: 'https://oauth.vk.com/authorize?client_id=6848088&display=page&response_type=code&scope=134479876&v=5.92',
       group_auth_link_to_get_access_token: 'https://oauth.vk.com/access_token?',
-      access_token: '0c42faa89e4ecf76ec401832257954c0622be8c0d4522cbfd56d829dde400bda489fc206768e5f3019a39'
+      user_code: '',
+      access_token: ''
     }
   }
 }
