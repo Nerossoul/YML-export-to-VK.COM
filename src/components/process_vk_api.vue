da44fe8c9ff47bcac31b573ef550a73af34693b7
Nerossoul <nerossoul@gmail.com>
2019-02-24 01:47:53 +0700

    exportAllProducts method is added

--- src/components/process_vk_api.vue
+++ src/components/process_vk_api.vue
@@ -1,28 +1,19 @@
 
 <template>
   <div class="card mt-3">
-      <h2>{{ msg }}</h2>
       <div class="row mt-2">
         <div class="col-sm">
           <div class="card">
-            <button type="button" class="btn btn-outline-secondary" @click="prepare_ProductData(500) ">Подготовить товар</button>
-          </div>
-        </div>
-        <div class="col-sm">
-          <div class="card">
-            <button type="button" class="btn btn-outline-secondary"  @click="upload_photoToVkGroupCommonMethod('https://playavto.ru/image/import_files/91/91ceb3bd-437a-11e8-bd9c-408d5c3aa21e_91ceb3bf-437a-11e8-bd9c-408d5c3aa21e.jpeg', 1) ">common file upload</button>
-          </div>
-        </div>
-        <div class="col-sm">
-          <div class="card">
-            <button type="button" class="btn btn-outline-secondary"  @click="isProductInMarket('000000021307')">Is product in market</button>
+            <button type="button" class="btn btn-outline-secondary"  @click="export_AllProductsToVk()">Export all products</button>
           </div>
         </div>
       </div>
       <br>
-      {{ response_text }}
+      <h5>{{ current_product.vendorCode }}</h5>
+      {{ current_product.name}}
       <hr>
-       <div v-for="(value, key) in prepared_product_data" :key="value.id" class="border border-primary mt-1"><h4>{{key}}</h4>{{value}}</div>
+      <h5>Progress text</h5>
+      <p style="white-space: pre-line">{{ action_string }}</p>
   </div>
 </template>
 
@@ -33,11 +24,15 @@ export default {
     name: 'vkApi',
   data () {
     return {
-      msg: 'vk',
       ownApiServer: 'https://playavto.ru/vk_export/server/',
       response_text: '',
-      sleeping_period: 400,
-      prepared_product_data: {}
+      sleeping_period: 2000,
+      sleeping_period_text: ' ⏸️️ Pause end. (ms)-',
+      prepared_product_data: {},
+      current_product: {},
+      action_string: '',
+      action_string_separator: `
+      `
     }
   },
   methods: {
@@ -113,7 +108,7 @@ export default {
     },
 
     market_edit(item_id,name,description,price,main_photo_id,photo_ids) {
-      let methodString = 'market.search'
+      let methodString = 'market.edit'
       let params = {
       'owner_id'      : '-' + this.$store.state.group_id,
       'item_id'       : item_id,
@@ -129,7 +124,7 @@ export default {
     },
 
     market_add(name,description,price,main_photo_id,photo_ids) {
-      let methodString = 'market.search'
+      let methodString = 'market.add'
       let params = {
         'owner_id'      : '-' + this.$store.state.group_id,
         'name'          : name,
@@ -142,29 +137,115 @@ export default {
       }
       return this._onwApi_call(methodString,params)
     },
+    
+    market_addToAlbum(market_item_id, album_id) {
+      let methodString = 'market.addToAlbum'
+      let params = {
+        'owner_id' : '-' + this.$store.state.group_id,
+        'item_id'  : market_item_id,
+        'album_ids' : album_id
+      }
+      return this._onwApi_call(methodString,params)
+    },
 
     isProductInMarket(model_id) {
       console.log('-Is ' + model_id + ' in market?')
       return new Promise(async (resolve, reject) => {
         let searchResult = await this.market_search(model_id) 
         resolve(searchResult)
-      }).then(searchResult=>{
+      })
+      .then(x => new Promise(resolve => setTimeout(() => {
+        console.log(this.sleeping_period_text + this.sleeping_period)
+        resolve(x)
+      }, this.sleeping_period)))
+      .then(searchResult=>{
         if (searchResult.response.count > 0) {
-      console.log('-YES! The ' + model_id + ' is in market')
-          return true
+          console.log('-YES! The ' + model_id + ' is in market')
+          this.logCurrentAction('Есть в группе. Запуск процесса обновления')
+          return searchResult.response.items[0].id
         } else {
-      console.log('-NO! The ' + model_id + ' isn\'t in market')
+          console.log('-NO! The ' + model_id + ' isn\'t in market')
+          this.logCurrentAction('Нет в группе. Запуск процесса добавления')
           return false
         }
       })
 
     },
     export_AllProductsToVk() {
-
+      let products = this.$store.state.products
+      let productCounter = 0
+      return new Promise(async (resolve, reject) => {
+        for (let product of products) {
+          this.current_product = product
+          console.log(productCounter, product.vendorCode);
+          let market_item_id = await this.isProductInMarket(product.vendorCode)
+          let productPrepariedData = await this.prepare_ProductData(productCounter)
+          console.log('Product data preparied. See below')
+          console.log(productPrepariedData)
+          let name = productPrepariedData.name
+          let description = productPrepariedData.description
+          let price = productPrepariedData.price
+          let main_photo_id = productPrepariedData.main_photo_id
+          let photo_ids = productPrepariedData.photo_ids
+          if (market_item_id != false) {
+            console.log( 'UPDATE PRODUCT! ' + market_item_id )
+            this.logCurrentAction('Отправляем запрос на изменение товара')
+            let response = await this.market_edit(market_item_id,name,description,price,main_photo_id,photo_ids)
+            if (response.response == 1) {
+              console.log('Product updated')
+              this.logCurrentAction('Успешно!')
+            } else {
+              console.log('Error update')
+              this.logCurrentAction('Oшибка: товар не обновлен!')
+            }
+          } else {
+            console.log('ADD PRODUCT!')
+            this.logCurrentAction('Отправляем запрос на добавление товара')
+            let response = await this.market_add(name,description,price,main_photo_id,photo_ids)
+            try {
+              if (response.response.market_item_id) {
+                console.log('Product added')
+                this.logCurrentAction('Успешно!')
+                await this.pause(this.sleeping_period)
+                // TODO market addToAlbum is here
+                let AlbumId = this.$store.state.CategoryToAlbumId[product.categoryId]
+                let market_item_id = response.response.market_item_id
+                console.log('Add this product to album')
+                this.logCurrentAction('Дoбавляем данный товар в подбоку')
+                let add_to_album_result = await this.market_addToAlbum(market_item_id, album_id)
+                if (add_to_album_result.response == 1) {
+                  console.log('Product added to album')
+                  this.logCurrentAction('Успешно добавлен в подборку!')
+                } else {
+                  console.log('Error add to album')
+                  this.logCurrentAction('Oшибка: товар не добавлен в подборку!')
+                }
+              } else {
+                console.log('Error: Product is not added')
+                this.logCurrentAction('Oшибка: товар не добавлен!')
+              }
+            } catch(error) {
+              console.log('Catch Error: Product is not added')
+              this.logCurrentAction('Критическая ошибка: товар не добавлен!')
+            }
+            
+          }
+          await this.pause(this.sleeping_period)
+          this.logCurrentAction() // очистить лог.
+          ++productCounter
+          document.title = 'VK EXPORT:( '+ productCounter +'/' +this.$store.state.products.length + ' )'
+          break
+        }
+        resolve('DONE!')
+      })
+      .then(result=> {
+        console.log(result)
+      })
     },
 
     prepare_ProductData(array_index) {
       return new Promise(async (resolve, reject) => {
+        this.logCurrentAction('Формируем запрос')
         let productPhotoUrls = this.$store.state.products[array_index].picture
         let photoCounter = 0
         let main_photo_id = ''
@@ -198,9 +279,9 @@ export default {
         let description = this.prepared_product_data.description
         description = description.replace( /\s{2,}/g, " " )
         description = 'Артикул товара: #' + vendorCode + ` 
-          %0A Наличие на складе: ` + onStore + `
-          %0A Ссылка на сайт: ` + productSiteUrl + '?utm_source=vk.com&utm_medium=group_shop&utm_campaign=vk.com_group_shop&utm_content=product_'+ vendorCode +`
-          %0A ОПИСАНИЕ ТОВАРА:
+          Наличие на складе: ` + onStore + `
+          Ссылка на сайт: ` + productSiteUrl + '?utm_source=vk.com&utm_medium=group_shop&utm_campaign=vk.com_group_shop&utm_content=product_'+ vendorCode +`
+          ОПИСАНИЕ ТОВАРА:
           `+ description
         let data = {
           'name'          : this.prepared_product_data.name,
@@ -212,6 +293,7 @@ export default {
           'photo_ids'     : photo_id_array[1],
         }
         this.prepared_product_data = data
+        this.logCurrentAction('Запрос сформирован')
         return data
       })
     },
@@ -219,24 +301,27 @@ export default {
     upload_photoToVkGroupCommonMethod(file_link) {
       console.log('----------------------------------------')
       console.log('Uploading new photo')
+      this.logCurrentAction('Loading photo')
       return new Promise(async (resolve, reject) => {
         console.log('Getting Upload Server')
         // TODO make reject scenario
         let response = await this.photos_getMarketUploadServer()
+        this.logCurrentAction('Photo:geting upload server ')
         resolve(response)
       })
       .then(x => new Promise(resolve => setTimeout(() => {
-        console.log('pause: '+ this.sleeping_period + " ms has been ended")
+        console.log(this.sleeping_period_text + this.sleeping_period)
         resolve(x)
         }, this.sleeping_period)))
       .then(async (result) => {
         // TODO make reject scenario
         console.log('Uploading photo to server')
+        this.logCurrentAction('Photo: Uploading...')
         let response = await this.upload_file(result.response.upload_url, file_link)
         return response
       })
       .then(x => new Promise(resolve => setTimeout(() => {
-        console.log('pause: '+ this.sleeping_period + " ms has been ended")
+        console.log(this.sleeping_period_text + this.sleeping_period)
         resolve(x)
         }, this.sleeping_period)))
       .then(async (uploaded_file_info) => {
@@ -247,15 +332,17 @@ export default {
         let crop_hash =uploaded_file_info.crop_hash
         // TODO make reject scenario
         console.log('Saving uploaded photo')
+        this.logCurrentAction('Photo: saving')
         let response = await this.photos_saveMarketPhoto(photo,server,hash,crop_data,crop_hash)
         return response
       })
       .then(x => new Promise(resolve => setTimeout(() => {
-        console.log('pause: '+ this.sleeping_period + " ms has been ended")
+        console.log(this.sleeping_period_text + this.sleeping_period)
         resolve(x)
         }, this.sleeping_period)))
       .then(saved_photo_info=>{
         let photo_id = saved_photo_info.response[0].id
+        this.logCurrentAction('OK')
         return photo_id
       })
     },
@@ -288,7 +375,25 @@ export default {
           return JSON.parse(json)
         }
       })
-    }
+    },
+
+    logCurrentAction(action_string) {
+      this.action_string += action_string + this.action_string_separator
+      if (arguments.length == 0) {
+        this.action_string = ''
+      }
+    },
+
+    pause(pausePeriod) {
+      return new Promise(async (resolve, reject) => {
+        console.log('Pause ' + pausePeriod + ' ms began')
+        setTimeout(()=>resolve('Pause end'), pausePeriod)
+      })
+      .then(pauseEndMessage=>{
+        console.log(pauseEndMessage)
+        return pauseEndMessage
+      })
+    },
   }
 }
 </script>
