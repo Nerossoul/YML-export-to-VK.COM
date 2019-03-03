167a652bf9a5acddc33ac361ff10bd4ffce16b26
Nerossoul <nerossoul@gmail.com>
2019-03-03 11:45:50 +0700

    delete real config.php from rep, passwords changed

--- static/api/config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-define('APP_ID', '6860578');
-define('APP_SECRET', 'XrNziJgBdL4lE1AjXCAv');
-define('REDIRECT_URI', 'https://playavto.ru/vk_export/static/api/vk_auth.php');
-// define('CLIENT_URI', 'http://localhost:8080/settings');
-define('CLIENT_URI', 'https://playavto.ru/vk_export/#/settings');
-define('VK_API_ENDPOINT', 'https://api.vk.com/method/');
-define('VK_API_VERSION', '5.92');
